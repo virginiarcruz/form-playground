43134e51ce39cd8084e6cf5c184094f1326affe0
virginia-cruz <virginia.cruz@hp.com>
2020-10-05 21:50:44 -0300

    add data api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import Home from './pages/Home';
-import { ApolloClient } from "@apollo/client";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
-
 
 import GlobalStyle from './styles/global';
 
-export const client = new ApolloClient({
-  uri: "http://localhost:4000"
-});
-
-
-
 const App: React.FC = () => {
   return (
-    <ApolloProvider client={client as ApolloClient<any>}>
+    <>
       <Home />
       <GlobalStyle />
-    </ApolloProvider>
+    </>
   );
 };
 
 export default App;
-
